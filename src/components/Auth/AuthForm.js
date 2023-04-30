ed78054bfa92ac68e427fdb776bfbda746b75ba9
AkashB07 <akashbadhi7@gmail.com>
2023-04-30 11:35:05 +0530

    Managing the token

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,10 +1,12 @@
-import { useState, useRef } from 'react';
-
+import { useState, useRef, useContext } from 'react';
+import AuthContext from '../store/auth-context';
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
+
+  const authCtx = useContext(AuthContext);
 
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
@@ -47,7 +49,7 @@ const AuthForm = () => {
       setIsLoading(false);
       if (respone.ok) {
         const data = await respone.json();
-        console.log(data);
+        authCtx.login(data.idToken);
       }
       else {
         const data = await respone.json()
